f63144bcd68fab8ec71f1a79c90f263cddcbca9a
carlosEA28 <carloseaduarte28@gmail.com>
2024-11-15 13:41:27 -0300

    first commit

---
+++ src/test/java/br/com/carlos/CardapioBackend/CardapioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlos.CardapioBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
